5d22e1066c0e360d93d60d5263a8d3ac345ceb57
PedroMiotti <PedroMiotti@users.noreply.github.com>
2023-03-19 18:46:55 +0000

    deploy: 5c13e59d11d5db3d9348f6b6a777a3f46bb2a2c5

--- _next/static/DoWXs6SiLGKv_VyHUnQOW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-bec26002a2c39819.js","static/chunks/284-a9cef2f7c7a3455f.js","static/chunks/pages/index-05464940b940f152.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
